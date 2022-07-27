7201b2120595f070183d0a6fd13fb06bebcfae58
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-27 17:46:17 +0200

    Fix 2

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ class Movie {
          } else{
             $this-> status = 'Old';
          }
-        return $this->status;
      }
      public function getStatus(){
         return $this->status;
@@ -27,6 +26,24 @@ class Movie {
 $titanic = new Movie('Titanic','Romance','1997');
 $titanic->getStatus();
 var_dump($titanic);
-var_dump($titanic->getStatus());
+
+$topGun = new Movie('Top Gun: Maverick', 'Action','2022');
+$topGun->getStatus();
+var_dump($topGun);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie List</title>
+</head>
+<body>
+    <div>
+        <h2><?php  ?></h2>
+    </div>
+</body>
+</html>
